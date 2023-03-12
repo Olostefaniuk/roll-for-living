94b39e574b32edcbd760edf1856798eaceda0d64
oloStefaniuk <stefaniuk.olek@gmail.com>
2023-03-12 14:51:33 +0100

    Added diceRoll function

--- routes/dungeon.ts
+++ routes/dungeon.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
+
 export const dungeonRouter = Router();
 
 dungeonRouter
   .get('/choose-form', (req, res) => {
     res.render('dungeon/choose-form');
+
   })
   .get('/:room', (req, res) => {
     const { room } = req.params;
---
+++ utils/diceRoll.ts
@@ -0,0 +1,1 @@
+export const diceRoll = (k: number): number => 1 + Math.floor(Math.random() * k);
